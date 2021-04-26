6a367c4d532af6db41dd2ea1d4cb0a135c76e0a1
1-blue <ghksaud55@naver.com>
2021-04-27 00:28:49 +0900

    complete mineSweeper only function

--- client/src/components/views/games/MineSweeper.vue
+++ client/src/components/views/games/MineSweeper.vue
@@ -1,7 +1,11 @@
 <template>
   <section id="mine__sweeper__container">
+    <div v-if="isClear" class="clear__message">
+      클리어시간 : {{ clear() }}초
+    </div>
+
     <!-- 게임난이도 입력받기 -->
-    <div>
+    <div class="level">
       <label for="horizontal">가로</label>
       <input type="number" name="horizontal" v-model="horizontal" min="1" max="25" />
       <label for="vertical">세로</label>
@@ -36,6 +40,8 @@ export default {
       board: [],      // 보드판
       kinds: {},      // 종류
       state: "",      // 현재 게임상태
+      startTime: 0,   // 게임클리어시간
+      openCount: 0,   // 열린칸 개수 ( 종료조건 )
       horizontal: 10, // 가로길이
       vertical: 10,   // 세로길이
       mineNumber: 10, // 지뢰개수
@@ -44,7 +50,9 @@ export default {
   methods: {
     init(){
       this.board = [];
+      this.openCount = 0;
       this.state = "awaiting";
+      this.startTime = 0;
       document.querySelectorAll(".piece").forEach(v => {
         v.classList.remove("open");
         v.innerText = "";
@@ -75,17 +83,15 @@ export default {
       //지뢰추가 ==== 여기 수정필요
       for (let i = 0; i < shuffle.length; i++) {
         const h = Math.floor(shuffle[i] / horizontal);
-        const v = shuffle[i] % vertical;
-
-        console.log(shuffle[i]);
-        console.log(h);
-        console.log(v);
+        const v = shuffle[i] % horizontal;
 
         this.board[h][v] = this.kinds.MINE;
       }
 
       // 게임시작
       this.state = "playing"
+
+      this.startTime = new Date;
     },
     leftClickPiece(e){
       // 게임진행중일경우에만 클릭가능
@@ -108,11 +114,16 @@ export default {
           e.currentTarget.classList.add("open");
 
           // 주위 지뢰개수
-          e.currentTarget.innerText = this.surroundMineNumber(v, h);
+          const surroundMineNumber = this.surroundMineNumber(v, h);
+          e.currentTarget.innerText = surroundMineNumber;
+          
+          // 주변칸 오픈... surroundMineNumber, surroundPieceOpen의 3번째 변수는 자기자신은 이미 처리했기때문에 예외적으로 처리해주기위해 사용...
+          if(surroundMineNumber === 0){
+            this.surroundPieceOpen(v, h, "exception");
+            this.openCount--;
+          }
 
-          // 주위칸오픈
           break;
-
         case 1:
           // 게임종료 및 답공개
           alert("지뢰 :: 게임종료");
@@ -157,25 +168,84 @@ export default {
           break;
       }
     },
-    surroundMineNumber(v, h){
+    surroundMineNumber(v, h, exception){
+      // 이미 열린칸이면 취소
+      if(this.board[v][h] === 2 && exception === undefined){
+        return;
+      }
+
+      // 오픈한거 board에 적용
+      this.board[v][h] = 2;
+
+      // 열린칸 개수세기
+      this.openCount++;
+
       const tempArray = [];
-    
-      try {
-        // 주변 조각들 배열에 넣고 ( 자기자신제외 )
+      // 주변 조각들 배열에 넣고 ( 자기자신제외 )
+      for(let vv = -1; vv <= 1; vv++){
+        for(let hh = -1; hh <= 1; hh++){
+          // 세로 인덱스범위초과제외
+          if(v + vv <= -1 || v + vv >= Number(this.vertical)){
+            continue;
+          }
+
+          // 가로 인덱스범위초과제외
+          if(h + hh <= -1 || h + hh >= Number(this.horizontal)){
+            continue;
+          }
+
+          // 자기자신제외
+          if(vv === 0 && hh === 0){
+            continue;
+          }
+          tempArray.push(this.board[v + vv][h + hh]);
+        }
+      }
+      // 지뢰인놈만 반환
+      const count = tempArray.filter(v => v === this.kinds.MINE);
+
+      // 지뢰개수리턴
+      return count.length;
+    },
+    surroundPieceOpen(v, h, exception){
+      if(this.board[v][h] === 2 && exception === undefined){
+        return;
+      }
+
+      const target = document.querySelector(".board").children[v].children[0].children[h];
+
+      target.classList.add("open");
+
+      const surroundMineNumber= this.surroundMineNumber(v, h, 123);
+      target.innerText = surroundMineNumber;
+
+      // 0이면 다시호출 (재귀함수)
+      if(surroundMineNumber === 0){
         for(let vv = -1; vv <= 1; vv++){
           for(let hh = -1; hh <= 1; hh++){
-            tempArray.push(this.board[v + vv][h + hh]);
+            // 세로 인덱스범위초과제외
+            if(v + vv <= -1 || v + vv >= Number(this.vertical)){
+              continue;
+            }
+
+            // 가로 인덱스범위초과제외
+            if(h + hh <= -1 || h + hh >= Number(this.horizontal)){
+              continue;
+            }
+
+            // 자기자신제외
+            if(vv === 0 && hh === 0){
+              continue;
+            }
+
+            this.surroundPieceOpen(v + vv, h + hh);
           }
         }
-      } catch (error) {
-        // 인덱스범위초과시 무시
-      } finally {
-        // 지뢰인놈만 반환
-        const count = tempArray.filter(v => v === this.kinds.MINE);
-
-        // 지뢰개수리턴
-        return count.length;
       }
+    },
+    clear(){
+      const time = (new Date() - this.startTime) / 1000
+      return time;
     }
   },
   computed: {
@@ -195,12 +265,22 @@ export default {
       }
 
       return false;
+    },
+    isClear(){
+      const horizontal = Number(this.horizontal);        // horizontal값
+      const vertical = Number(this.vertical);        // vertical값
+      const mineNumber = Number(this.mineNumber);        // vertical값
+
+      return this.openCount === (horizontal * vertical - mineNumber);
     }
   },
   created(){
     this.state = "waiting";
-    // 0, O : 아무것도없음
-    // X, 1 : 마인
+    /**
+     * 0, O : 아무것도없음
+     * X, 1 : 마인
+     * 2 : 오픈
+     */
     this.kinds.FLAG = 0;
     this.kinds.MINE = 1;
   }
@@ -213,6 +293,24 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+#mine__sweeper__container > div{
+  margin-bottom: 2rem;
+}
+
+@keyframes clear_message{
+  70%{
+    font-size: 2.5rem;
+  }
+  to{
+    font-size: 2rem;
+  }
+}
+
+.clear__message{
+  animation: clear_message 1s forwards ease-in-out;
+  font-size: 0;
 }
 
 .board{
